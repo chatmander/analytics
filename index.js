05d3ec2b051699377df3962f48c56e42dff89e94
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-10-11 02:29:43 +0530

    starting

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const axios = require("axios");
+
+const PORT = process.env.PORT || 5000;
+
+setInterval(() => {
+    axios
+        .get("https://genesis-chat-box.herokuapp.com")
+        .then(() => {})
+        .catch(() => {});
+}, 600000);
+
+app.get("/", async (req, res) => {
+    // add time and number of users
+    fs.readFile("analytics.json", "utf8", (err, data) => {
+        if (err) return res.status(500).send("bad!");
+        data = JSON.parse(data);
+
+        if(Date.now() - data[data.length - 1] < 3600) {
+            data[data.length - 1].users += 1;
+        }
+        else {
+            const obj = {
+                time: Date.now(),
+                users: 1
+            }
+            data.push(obj);
+        }
+        
+        fs.writeFile("contacts.json", JSON.stringify(data), (err) => {
+            if (err) return res.status(500).send("bad!");
+            return res.status(201).send("successful");
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server connected to port ${PORT}`);
+});
